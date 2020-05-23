d846fe40c8a2eff0355b19b547d53275c941cd13
emrerdem1 <emrerdem94@gmail.com>
2020-05-24 02:57:31 +0300

    Done.

--- src/Cell.js
+++ src/Cell.js
@@ -1,19 +1,23 @@
 import React, { Component } from 'react';
 
 export default class Cell extends Component {
-  
+
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       color: this.props.color
     }
+    this.modifyColor2 = this.modifyColor2.bind(this);
   }
-  
+  modifyColor2 = () => {
+    this.setState({
+      color: this.props.clickedColor
+    })
+  }
   render() {
     return (
-      <div className="cell" style={{backgroundColor: this.state.color}}>
+      <div className="cell" onClick={this.modifyColor2} style={{ backgroundColor: this.state.color }}>
       </div>
     )
   }
-  
 }
--- src/ColorSelector.js
+++ src/ColorSelector.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
 
 export default class ColorSelector extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      color: 'red'
+    }
+    this.modifyColor = this.modifyColor.bind(this);
+  }
+
+  modifyColor = (e) => this.props.updateKey(e.target.style.backgroundColor);
 
   makeColorSwatches = () => (
     ["#F00", "#F80", "#FF0", "#0F0", "#00F", "#508", "#90D", "#FFF", "#000"].map((str, idx) => {
-      return <div key={idx} className="color-swatch" style={{backgroundColor: str}}/>
+      return <div key={idx} className="color-swatch" style={{ backgroundColor: str }} onClick={this.modifyColor} />
     })
   )
 
@@ -16,3 +25,27 @@ export default class ColorSelector extends Component {
     )
   }
 }
+/*
+function Parent() {
+  const [color, setColor] = useState('');
+  function handleChildClick(color) {
+    setColor(color);
+  }
+  return (
+    <>
+      <h1>selected color: {color}</h1>
+      {['green','red','blue'].map((color) => (
+        <Child color={color} onChildClick={handleChildClick} ... />
+      ))}
+    </>
+  )
+}
+function Child({ color,  onChildClick }) {
+  function handleClick(event) {
+    onChildClick(event.target.name); // pass any argument to the callback
+  }
+  return (
+    <button name={color} onClick={handleClick}>{color}</button>
+  )
+}
+*/
